364bee6ce33a8f9b7c0583049ceb5351956779a9
kash2005 <kashmifernando3@gmail.com>
2024-05-13 13:58:51 +0530

    init the project

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function vertify(emp) {
+    // vertifying employee.... 
+}
+let msg = "hello";
+exports.default = {};
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+interface Employee {
+    id : number,
+    name : string
+}
+
+function vertify(emp : Employee) {
+   // vertifying employee.... 
+}
+let msg = "hello";
+export default{}
